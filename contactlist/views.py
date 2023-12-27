c3defcfd34d6fa93d26ee779e5601ec5c79d74bf
style77 <stylek777@gmail.com>
2023-12-27 02:05:22 +0100

    chore: homeview and successview

--- contactlist/views.py
+++ contactlist/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView, TemplateView
+from contactlist.forms import UploadContactForm
 
-# Create your views here.
+
+class HomeView(FormView):
+    template_name = "home.html"
+    form_class = UploadContactForm
+    success_url = reverse_lazy('success_page')
+
+    def form_valid(self, form):
+        form.handle_file(self.request)
+        return super().form_valid(form)
+
+
+class SuccessView(TemplateView):
+    template_name = "success.html"
