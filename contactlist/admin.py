47f73ae0f3c62f712402785d8078a4d2a9f42276
style77 <stylek777@gmail.com>
2023-12-27 02:04:32 +0100

    chore: add models to admin panel

--- contactlist/admin.py
+++ contactlist/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from contactlist.models import Contact, ContactList
 
 # Register your models here.
+
+admin.site.register(ContactList)
+admin.site.register(Contact)
