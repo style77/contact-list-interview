61140300bb633f9c4e7b07b30d3012c81d89bec0
style77 <stylek777@gmail.com>
2023-12-27 01:27:00 +0100

    chore: contact and contactlist model

--- contactlist/models.py
+++ contactlist/models.py
@@ -1,14 +1,10 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-
-
-User = get_user_model()
+from contactlist.validators import validate_file_extension
 
 
 class ContactList(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(...)
-    import_file = models.FileField(...)
+    name = models.CharField()
+    import_file = models.FileField(upload_to="contact_lists/", validators=[validate_file_extension])
     processed = models.BooleanField(default=False)
 
 
